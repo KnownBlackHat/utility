9d871af06885aee1a4865e1c64d8659b95e44a2f
乇THɨ匚AŁ乄HA匚ʞ乇尺 <65008967+KnownBlackHat@users.noreply.github.com>
2021-12-18 00:24:00 +0530

    Update sitting_arragement.py

--- sitting_arragement.py
+++ sitting_arragement.py
@@ -1,11 +1,13 @@
 import random
+import os
 Genders=['Boys','Girls']
 Seats=['Window Side','Wall Side']
 date=input('Date: ')
 subject=input('Subject: ')
 timing=input('time: ')
-print('-'*10)
-print(f''' *Date:* {date}
+os.clear()
+print(f'''
+*Date:* {date}
 *Subject:* {subject}
 *Timing:* {timing}
 *Sitting Arrangement:* Window Side-{random.choice(Genders)}''')
